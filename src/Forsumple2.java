7ef38e350da8045e59d857eec32bc90562652a3b
38say <owner@localhost>
2024-10-21 23:03:28 +0900

    lesson4

---
+++ src/Forsumple2.java
@@ -0,0 +1,17 @@
+
+public class Forsumple2 {
+
+    public static void main(String[] args) {
+        //iの値が0から21より小さい間処理を繰り返す
+        for(int i =0 ; i < 21 ; i++) {
+            //iの値がで割り切れる時
+            if(i % 2 == 0) {
+                //iの値を表示する
+                System .out .println("i = "  + i);
+
+            }
+        }
+
+    }
+
+}
