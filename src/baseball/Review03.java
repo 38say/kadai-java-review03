3eac7854a7eb9fc44f30ac0074ec652f8ddf7241
38say <owner@localhost>
2024-10-26 18:09:02 +0900

    課題

---
+++ src/baseball/Review03.java
@@ -0,0 +1,28 @@
+package baseball;
+
+public class Review03 {
+
+    public static void main(String[] args) {
+        BaseBallTeam A= new BaseBallTeam("東京ヤクルトスワローズ",80,59,4);
+
+        BaseBallTeam B= new BaseBallTeam("横浜DeNaベイスターズ",73,68,2);
+
+        BaseBallTeam C= new BaseBallTeam("阪神タイガース",68,71,4);
+
+        BaseBallTeam D= new BaseBallTeam("読売ジャイアンツ",68,72,3);
+
+        BaseBallTeam E= new BaseBallTeam("広島東洋カープ",66,74,3);
+
+        BaseBallTeam F= new BaseBallTeam("中日ドラゴンズ",66,75,2);
+
+        A.report();
+        B.report();
+        C.report();
+        D.report();
+        E.report();
+        F.report();
+
+    }
+
+
+    }
